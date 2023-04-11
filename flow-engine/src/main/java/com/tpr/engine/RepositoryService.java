85bdf6cd8ee64cf61d95b27c4f7abf7ac46a4885
canghaihongxin <xxx@xxx.com>
2023-04-11 13:49:42 +0800

    项目初始化

---
+++ flow-engine/src/main/java/com/tpr/engine/RepositoryService.java
@@ -0,0 +1,10 @@
+package com.tpr.engine;
+
+/**
+ * @author canghaihongxin
+ * @version 1.0
+ * @description: 提供对流程定义和部署存储库的访问权限的服务。
+ * @date 2023/4/11 13:48
+ */
+public interface RepositoryService {
+}
